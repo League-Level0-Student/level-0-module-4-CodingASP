63c77524c9947085a82189c0c975cac6fd697184
CodingASP <78342170+CodingASP@users.noreply.github.com>
2022-08-27 14:36:50 -0700

    8/27

--- src/_01_double/_2_test_scores/DoubleTestScores.java
+++ src/_01_double/_2_test_scores/DoubleTestScores.java
@@ -1,5 +1,18 @@
 package _01_double._2_test_scores;
 
+import javax.swing.JOptionPane;
+
 public class DoubleTestScores {
 
+	String score = JOptionPane.showInputDialog("Enter a test score that has a decimal");
+	int number = Integer.parseInt(score);
+	
+	if (number > 100) {
+	JOptionPane.showMessageDialog(null, "Outstanding!");
+	//ask for help next class
+	}
 }
+}
+
+
+	
