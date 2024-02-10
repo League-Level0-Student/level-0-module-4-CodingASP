a25b7f81c2bd98cd1023b314e6c6f0f8c97de684
CodingASP <78342170+CodingASP@users.noreply.github.com>
2024-02-10 13:10:33 -0800

    2/10/24 - Everything is on my other pc

--- src/_02_boolean/_3_game_over/GameOver.java
+++ src/_02_boolean/_3_game_over/GameOver.java
@@ -10,14 +10,18 @@ public class GameOver {
 		
 		// 1. Ask the user if the game is over and save their answer.
 String response = JOptionPane.showInputDialog("Is the game over?");
-int ans = (response);
+		while(gameOver != true)
+		{
 		// 2. If they answer "yes" change gameOver to true.
-		if (ans == 'yes');
+		if (response.equals("yes"));
+			gameOver = true;
+			
+		}
 		// 3. Surround steps 1-2 in a while loop that runs as long as
 		//    gameOver is false. Hint: Use the ! operator.
 		
 		// 4. Tell the user the game is over outside the while loop.
-		
+		JOptionPane.showMessageDialog(null, "The game is over");
 	}
 
 }
